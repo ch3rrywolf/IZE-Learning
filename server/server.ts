77a60f737fe24a627088503961b8a647b7e4ada9
ch3rrywolf <ch3rrywolf111@gmail.com>
2025-07-25 22:47:50 +0100

    {IZE-LEARNING} body, cookie, cors, testing api, unknown route {ts}

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,7 @@
-import {app} from './app';
+import { app } from './app';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-// create server
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
