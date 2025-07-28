258a1ae505efb46f27b36357b26416dd34e7b626
ch3rrywolf <ch3rrywolf111@gmail.com>
2025-07-29 00:03:32 +0100

    {IZE-LEARNING} controllers,utils,models user.controller loginUser / jwt {ts}

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -8,6 +8,7 @@ import jwt, { Secret } from "jsonwebtoken";
 import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
+import { sendToken } from "../utils/jwt";
 
 // register user
 interface IRegisterationBody {
@@ -129,3 +130,35 @@ export const activateUser = CatchAsyncError(
     }
   }
 );
+
+
+// login user
+interface ILoginRequest {
+  email: string;
+  password: string;
+}
+
+export const loginUser = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body as ILoginRequest;
+
+    if (!email || !password) {
+      return next(new ErrorHandler("Please provide email and password", 400));
+    }
+
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (!user) {
+      return next(new ErrorHandler("Invalid email or password", 400));
+    };
+
+    const isPasswordMatch = await user.comparePassword(password);
+    if (!isPasswordMatch) {
+      return next(new ErrorHandler("Invalid email or password", 400));
+    };
+
+    sendToken(user, 200, res);
+  }  catch (error: any) {
+    return next(new ErrorHandler(error.message, 400));
+  }
+});
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { activateUser, registerationUser } from '../controllers/user.controller';
+import { activateUser, loginUser, registerationUser } from '../controllers/user.controller';
 const userRouter = express.Router();
 
 userRouter.post('/registration', registerationUser);
 userRouter.post('/activate-user', activateUser);
+userRouter.post('/login', loginUser);
 
 export default userRouter;
