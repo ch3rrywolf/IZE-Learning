3023df34c99c94e4dbccaf27eaf4156f0b0b9735
ch3rrywolf <ch3rrywolf111@gmail.com>
2025-07-29 00:31:39 +0100

    {IZE-LEARNING} controllers user.controller logoutUser {ts}

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { activateUser, loginUser, registerationUser } from '../controllers/user.controller';
+import { activateUser, loginUser, logoutUser, registerationUser } from '../controllers/user.controller';
 const userRouter = express.Router();
 
 userRouter.post('/registration', registerationUser);
 userRouter.post('/activate-user', activateUser);
 userRouter.post('/login', loginUser);
+userRouter.get('/logout', logoutUser);
 
 export default userRouter;
