8561784bb9d46683e88db1c1f0024fe0bfa6929d
ch3rrywolf <ch3rrywolf111@gmail.com>
2025-07-29 10:36:40 +0200

    {IZE-LEARNING} controllers user.controller getUserInfo {ts}

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -10,6 +10,7 @@ import path from "path";
 import sendMail from "../utils/sendMail";
 import { accessTokenOptions, refreshTokenOptions, sendToken } from "../utils/jwt";
 import { redis } from "../utils/redis";
+import { getUserById } from "../services/user.service";
 
 // register user
 interface IRegisterationBody {
@@ -237,6 +238,19 @@ export const updateAccessToken = CatchAsyncError(
 
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+// get user info
+export const getUserInfo = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = req.user?._id;
+      getUserById(userId, res);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+
     }
   }
 );
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { activateUser, loginUser, logoutUser, registerationUser, updateAccessToken } from '../controllers/user.controller';
+import { activateUser, getUserInfo, loginUser, logoutUser, registerationUser, updateAccessToken } from '../controllers/user.controller';
 import { authorizeRoles, isAutheticated } from '../middleware/auth';
 const userRouter = express.Router();
 
@@ -9,6 +9,7 @@ userRouter.post('/activate-user', activateUser);
 userRouter.post('/login', loginUser);
 userRouter.get('/logout', isAutheticated, authorizeRoles("admin"), logoutUser);
 userRouter.get('/refresh', updateAccessToken);
+userRouter.get('/me', isAutheticated, getUserInfo);
 
 
 export default userRouter;
